959363586a5391b65d2ad0c409d291758f36045a
tunamaguro <tsu7magu6@gmail.com>
2024-06-16 14:16:01 +0000

    2-2:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn add(stack: &mut Vec<i32>) {
+    let left = stack.pop().unwrap();
+    let right = stack.pop().unwrap();
+    stack.push(left + right)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut stack = vec![];
+
+    stack.push(32);
+    stack.push(44);
+
+    add(&mut stack);
+
+    stack.push(100);
+    add(&mut stack);
+
+    println!("Stack: {stack:?}")
 }
