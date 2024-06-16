b9c8994497b46ece993f22b9169d09630d6c9754
tunamaguro <tsu7magu6@gmail.com>
2024-06-16 14:37:34 +0000

    標準入力から読み込み

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
 fn add(stack: &mut Vec<i32>) {
-    let left = stack.pop().unwrap();
     let right = stack.pop().unwrap();
+    let left = stack.pop().unwrap();
     stack.push(left + right)
 }
 
+fn sub(stack: &mut Vec<i32>) {
+    let right = stack.pop().unwrap();
+    let left = stack.pop().unwrap();
+    stack.push(left - right)
+}
+
+fn mul(stack: &mut Vec<i32>) {
+    let right = stack.pop().unwrap();
+    let left = stack.pop().unwrap();
+    stack.push(left * right)
+}
+
+fn div(stack: &mut Vec<i32>) {
+    let right = stack.pop().unwrap();
+    let left = stack.pop().unwrap();
+    stack.push(left / right)
+}
+
 fn main() {
-    let mut stack = vec![];
-
-    stack.push(32);
-    stack.push(44);
-
-    add(&mut stack);
-
-    stack.push(100);
-    add(&mut stack);
-
-    println!("Stack: {stack:?}")
+    for line in std::io::stdin().lines() {
+        let mut stack = vec![];
+        if let Ok(line) = line {
+            let tokens = line.split(' ').collect::<Vec<_>>();
+            for token in tokens {
+                if let Ok(num) = token.parse::<i32>() {
+                    stack.push(num);
+                } else {
+                    match token {
+                        "+" => add(&mut stack),
+                        "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
+                        _ => unreachable!(),
+                    }
+                }
+            }
+        }
+        println!("Stack: {stack:?}")
+    }
 }
