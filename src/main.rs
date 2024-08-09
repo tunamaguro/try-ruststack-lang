84636002319dfb73ae863c2b3073ade43625097d
tunamaguro <tsu7magu6@gmail.com>
2024-08-09 08:46:22 +0000

    ざっくりエラーを取っていたらこんな感じ?

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,93 @@
-fn add(stack: &mut Vec<i32>) {
-    let right = stack.pop().unwrap();
-    let left = stack.pop().unwrap();
-    stack.push(left + right)
+#[derive(Debug, PartialEq, Eq)]
+enum Value<'src> {
+    Num(i32),
+    Op(&'src str),
+    Block(Vec<Value<'src>>),
 }
 
-fn sub(stack: &mut Vec<i32>) {
-    let right = stack.pop().unwrap();
-    let left = stack.pop().unwrap();
-    stack.push(left - right)
+impl<'src> Value<'src> {
+    fn as_num(&self) -> i32 {
+        match self {
+            Self::Num(val) => *val,
+            _ => panic!("not number"),
+        }
+    }
 }
 
-fn mul(stack: &mut Vec<i32>) {
-    let right = stack.pop().unwrap();
-    let left = stack.pop().unwrap();
-    stack.push(left * right)
+fn add(stack: &mut Vec<Value>) {
+    let right = stack.pop().unwrap().as_num();
+    let left = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(left + right))
 }
 
-fn div(stack: &mut Vec<i32>) {
-    let right = stack.pop().unwrap();
-    let left = stack.pop().unwrap();
-    stack.push(left / right)
+fn sub(stack: &mut Vec<Value>) {
+    let right = stack.pop().unwrap().as_num();
+    let left = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(left - right))
+}
+
+fn mul(stack: &mut Vec<Value>) {
+    let right = stack.pop().unwrap().as_num();
+    let left = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(left * right))
+}
+
+fn div(stack: &mut Vec<Value>) {
+    let right = stack.pop().unwrap().as_num();
+    let left = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(left / right))
+}
+
+fn parse_block<'src, 'a>(input: &'a [&'src str]) -> (Value<'src>, &'a [&'src str]) {
+    let mut tokens = vec![];
+    let mut words = input;
+
+    while let Some((&word, mut rest)) = words.split_first() {
+        if word.is_empty() {
+            break;
+        }
+        if word == "{" {
+            let value;
+            (value, rest) = parse_block(rest);
+            tokens.push(value);
+        } else if word == "}" {
+            return (Value::Block(tokens), rest);
+        } else if let Ok(value) = word.parse::<i32>() {
+            tokens.push(Value::Num(value))
+        } else {
+            tokens.push(Value::Op(word));
+        }
+
+        words = rest;
+    }
+    (Value::Block(tokens), words)
 }
 
 fn main() {
-    for line in std::io::stdin().lines() {
+    for line in std::io::stdin().lines().map_while(Result::ok) {
         let mut stack = vec![];
-        if let Ok(line) = line {
-            let tokens = line.split(' ').collect::<Vec<_>>();
-            for token in tokens {
-                if let Ok(num) = token.parse::<i32>() {
-                    stack.push(num);
-                } else {
-                    match token {
-                        "+" => add(&mut stack),
-                        "-" => sub(&mut stack),
-                        "*" => mul(&mut stack),
-                        "/" => div(&mut stack),
-                        _ => unreachable!(),
-                    }
+        let input: Vec<_> = line.split(' ').collect();
+        let mut words = &input[..];
+        while let Some((&word, mut rest)) = words.split_first() {
+            if word == "{" {
+                // ブロックのパース
+                let value;
+                (value, rest) = parse_block(rest);
+                stack.push(value);
+            } else if let Ok(val) = word.parse::<i32>() {
+                // 数値のパース
+                stack.push(Value::Num(val));
+            } else {
+                // 演算子のパース
+                match word {
+                    "+" => add(&mut stack),
+                    "-" => sub(&mut stack),
+                    "*" => mul(&mut stack),
+                    "/" => div(&mut stack),
+                    _ => unreachable!(),
                 }
             }
+            words = rest;
         }
         println!("Stack: {stack:?}")
     }
