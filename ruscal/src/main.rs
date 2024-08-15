0769c357b7e89f6172dbbc2821203618855ff2a4
tunamaguro <tsu7magu6@gmail.com>
2024-08-15 17:12:11 +0000

    token

--- ruscal/src/main.rs
+++ ruscal/src/main.rs
@@ -1,3 +1,9 @@
+#[derive(Debug, PartialEq, Eq)]
+enum Token {
+    Ident,
+    Number,
+}
+
 fn whitespace(mut input: &str) -> &str {
     while matches!(input.chars().next(), Some(' ')) {
         let mut chars = input.chars();
@@ -7,16 +13,18 @@ fn whitespace(mut input: &str) -> &str {
     input
 }
 
-fn number(mut input: &str) -> &str {
+fn number(mut input: &str) -> (&str, Option<Token>) {
     let mut chars = input.chars();
     if matches!(chars.next(), Some(_x @ ('-' | '+' | '.' | '0'..='9'))) {
         while matches!(chars.next(), Some(_x @ ('.' | '0'..='9'))) {
             input = chars.as_str();
         }
+        (input, Some(Token::Number))
+    } else {
+        (input, None)
     }
-    input
 }
-fn ident(mut input: &str) -> &str {
+fn ident(mut input: &str) -> (&str, Option<Token>) {
     if matches!(input.chars().next(), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
         while matches!(
             input.chars().next(),
@@ -26,8 +34,35 @@ fn ident(mut input: &str) -> &str {
             chars.next();
             input = chars.as_str();
         }
+        (input, Some(Token::Ident))
+    } else {
+        (input, None)
     }
-    input
+}
+
+fn token(input: &str) -> (&str, Option<Token>) {
+    if let (input, Some(ident_res)) = ident(whitespace(input)) {
+        return (input, Some(ident_res));
+    }
+
+    if let (input, Some(number_res)) = number(whitespace(input)) {
+        return (input, Some(number_res));
+    }
+
+    (input, None)
+}
+
+fn source(mut input: &str) -> Vec<Token> {
+    let mut tokens = vec![];
+    while !input.is_empty() {
+        input = if let (next_input, Some(token)) = token(input) {
+            tokens.push(token);
+            next_input
+        } else {
+            break;
+        }
+    }
+    tokens
 }
 
 fn main() {
@@ -45,11 +80,19 @@ mod tests {
 
     #[test]
     fn test_number() {
-        assert_eq!(number("+1.123 "), " ");
+        assert_eq!(number("+1.123 "), (" ", Some(Token::Number)));
     }
 
     #[test]
     fn test_ident() {
-        assert_eq!(ident("AbcDE0123"), "");
+        assert_eq!(ident("AbcDE0123"), ("", Some(Token::Ident)));
+    }
+
+    #[test]
+    fn test_source() {
+        assert_eq!(
+            source("    aaaa -123 ko090"),
+            vec![Token::Ident, Token::Number, Token::Ident]
+        )
     }
 }
