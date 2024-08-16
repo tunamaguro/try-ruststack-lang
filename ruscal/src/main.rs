2facd8c8486f6bc53d73d3fe27ef43e97b40e939
tunamaguro <tsu7magu6@gmail.com>
2024-08-16 16:21:12 +0000

    木構造に変換

--- ruscal/src/main.rs
+++ ruscal/src/main.rs
@@ -1,7 +1,15 @@
-#[derive(Debug, PartialEq, Eq)]
-enum Token {
-    Ident,
-    Number,
+#[derive(Debug, PartialEq)]
+enum Token<'src> {
+    Ident(&'src str),
+    Number(f64),
+    LParen,
+    RParen,
+}
+
+#[derive(Debug, PartialEq)]
+enum TokenTree<'src> {
+    Token(Token<'src>),
+    Tree(Vec<TokenTree<'src>>),
 }
 
 fn advance_char(input: &str) -> &str {
@@ -21,18 +29,24 @@ fn whitespace(mut input: &str) -> &str {
     input
 }
 
-fn number(mut input: &str) -> (&str, Option<Token>) {
+fn number(mut input: &str) -> Option<(&str, Token)> {
+    let start = input;
     if matches!(peek_char(input), Some(_x @ ('-' | '+' | '.' | '0'..='9'))) {
         input = advance_char(input);
         while matches!(peek_char(input), Some(_x @ ('.' | '0'..='9'))) {
             input = advance_char(input);
         }
-        (input, Some(Token::Number))
+    };
+
+    if let Ok(num) = start[..start.len() - input.len()].parse::<f64>() {
+        Some((input, Token::Number(num)))
     } else {
-        (input, None)
+        None
     }
 }
-fn ident(mut input: &str) -> (&str, Option<Token>) {
+
+fn ident(mut input: &str) -> Option<(&str, Token)> {
+    let start = input;
     if matches!(peek_char(input), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
         input = advance_char(input);
         while matches!(
@@ -41,39 +55,79 @@ fn ident(mut input: &str) -> (&str, Option<Token>) {
         ) {
             input = advance_char(input)
         }
-        (input, Some(Token::Ident))
+        Some((input, Token::Ident(&start[..start.len() - input.len()])))
     } else {
-        (input, None)
+        None
     }
 }
 
-fn token(input: &str) -> (&str, Option<Token>) {
-    if let (input, Some(ident_res)) = ident(whitespace(input)) {
-        return (input, Some(ident_res));
+fn lparen(mut input: &str) -> Option<(&str, Token)> {
+    if matches!(peek_char(input), Some('(')) {
+        input = advance_char(input);
+        Some((input, Token::LParen))
+    } else {
+        None
     }
-
-    if let (input, Some(number_res)) = number(whitespace(input)) {
-        return (input, Some(number_res));
-    }
-
-    (input, None)
 }
 
-fn source(mut input: &str) -> Vec<Token> {
+fn rparen(mut input: &str) -> Option<(&str, Token)> {
+    if matches!(peek_char(input), Some(')')) {
+        input = advance_char(input);
+        Some((input, Token::RParen))
+    } else {
+        None
+    }
+}
+
+fn token(input: &str) -> Option<(&str, Token)> {
+    if let Some(ident_res) = ident(whitespace(input)) {
+        return Some(ident_res);
+    }
+
+    if let Some(number_res) = number(whitespace(input)) {
+        return Some(number_res);
+    }
+
+    if let Some(lparen_res) = lparen(whitespace(input)) {
+        return Some(lparen_res);
+    }
+
+    if let Some(rparen_res) = rparen(whitespace(input)) {
+        return Some(rparen_res);
+    }
+
+    None
+}
+
+fn source(mut input: &str) -> (&str, TokenTree) {
     let mut tokens = vec![];
     while !input.is_empty() {
-        input = if let (next_input, Some(token)) = token(input) {
-            tokens.push(token);
-            next_input
+        input = if let Some((next_input, token)) = token(input) {
+            match token {
+                Token::LParen => {
+                    let (next_input, tt) = source(next_input);
+                    tokens.push(tt);
+                    next_input
+                }
+                Token::RParen => {
+                    return (next_input, TokenTree::Tree(tokens));
+                }
+                _ => {
+                    tokens.push(TokenTree::Token(token));
+                    next_input
+                }
+            }
         } else {
             break;
         }
     }
-    tokens
+    (input, TokenTree::Tree(tokens))
 }
 
 fn main() {
-    println!("Hello, world!");
+    let s = "(123 456 world)";
+    println!("source: {:?}", s);
+    println!("parsed: {:?}", source(s));
 }
 
 #[cfg(test)]
@@ -87,19 +141,26 @@ mod tests {
 
     #[test]
     fn test_number() {
-        assert_eq!(number("+1.123 "), (" ", Some(Token::Number)));
+        assert_eq!(number("+1.123 "), Some((" ", Token::Number(1.123))));
     }
 
     #[test]
     fn test_ident() {
-        assert_eq!(ident("AbcDE0123"), ("", Some(Token::Ident)));
+        assert_eq!(ident("AbcDE0123"), Some(("", Token::Ident("AbcDE0123"))));
     }
 
     #[test]
     fn test_source() {
         assert_eq!(
             source("    aaaa -123 ko090"),
-            vec![Token::Ident, Token::Number, Token::Ident]
+            (
+                "",
+                TokenTree::Tree(vec![
+                    TokenTree::Token(Token::Ident("aaaa")),
+                    TokenTree::Token(Token::Number(-123.0)),
+                    TokenTree::Token(Token::Ident("ko090"))
+                ])
+            )
         )
     }
 }
